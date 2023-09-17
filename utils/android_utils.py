a923c122403d18ee034d2e9fde3e12d310c2b52a
Redr1ghthand <4aikaman@gmail.com>
2023-09-17 19:25:37 +0300

    New function get_android_udid()  Unique Device Identifier of an android device by executing the  'adb device' command using subprocess module. It returns data UDID  of the first connected device or None.

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,15 +1,44 @@
+import subprocess
+
+
+def get_android_udid():
+    result = subprocess.check_output(["adb", "devices"]).decode("utf-8")
+    lines = result.strip().split("\n")
+    devices = [line.split("\t")[0] for line in lines[1:] if "device" in line]
+    if devices:
+        return devices[0]
+    else:
+        return None
+
+
 def android_get_desired_capabilities():
-    return {
-        'autoGrantPermissions': True,
-        'automationName': 'uiautomator2',
-        'newCommandTimeout': 500,
-        'noSign': True,
-        'deviceName': '6926d9cb',
-        'platformName': 'Android',
-        'platformVersion': '10',
-        'resetKeyboard': True,
-        'systemPort': 8301,
-        'takesScreenshot': True,
-        'appPackage': 'com.ajaxsystems',
-        'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
-    }
+    udid = get_android_udid()
+    if udid:
+        return {
+            'autoGrantPermissions': True,
+            'automationName': 'uiautomator2',
+            'newCommandTimeout': 500,
+            'noSign': True,
+            'platformName': 'Android',
+            'platformVersion': '10',
+            'resetKeyboard': True,
+            'systemPort': 8301,
+            'takesScreenshot': True,
+            'udid': udid,
+            'appPackage': 'com.ajaxsystems',
+            'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
+        }
+    else:
+        return {
+            'autoGrantPermissions': True,
+            'automationName': 'uiautomator2',
+            'newCommandTimeout': 500,
+            'noSign': True,
+            'platformName': 'Android',
+            'platformVersion': '10',
+            'resetKeyboard': True,
+            'systemPort': 8301,
+            'takesScreenshot': True,
+            'appPackage': 'com.ajaxsystems',
+            'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
+        }
